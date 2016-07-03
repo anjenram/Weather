201d02a88d30b30739a5882090cede211f2bcf19
Anaty <Shumilov-anato@yandex.ru>
2016-07-04 02:07:43 +0300

    Refactoring controller.js , Add readme.md and comments in index.html, Fix style.css

--- controller.js
+++ controller.js
@@ -1,121 +1,117 @@
-var app = angular.module('myApp', []);
-app.factory('WeatherApi', function ($http) {
-	var obj = {};
+(function () {
+    var app = angular
+        .module('myApp', [])
+        .factory('WeatherApi', WeatherApi)
+        .controller('MainCtrl', MainCtrl);
 
-	obj.getLoc = function () {
-		return $http.jsonp("http://ipinfo.io/json?callback=JSON_CALLBACK");
-	};
-	obj.getWeather = function (latitude, longitude) {
-		var api = "http://api.openweathermap.org/data/2.5/weather?";
-		var units = "&units=metric";
-		var appid = '&appid=270245b93dc95a2783a7a4937a627994';
-		var cb = "&callback=JSON_CALLBACK";
+    function WeatherApi($http) {
+        var weatherApiUrl = "http://api.openweathermap.org/data/2.5/weather";
+        var obj = {};
 
-		return $http.jsonp(api + latitude + longitude + units + appid + cb);
-	};
+        obj.getLoc = function () {
+            return $http.jsonp("http://ipinfo.io/json?callback=JSON_CALLBACK");
+        };
+        obj.getWeather = function (latitude, longitude, degreeType) {
+            var requestConfig = {
+                params: {
+                    lat: latitude,
+                    lon: longitude,
+                    appid: '270245b93dc95a2783a7a4937a627994',
+                    callback: "JSON_CALLBACK"
+                }
+            };
 
-	return obj;
-});
+            if (degreeType && degreeType === degreeSystem.cel) {
+                requestConfig.params.units = 'metric';
+            };
 
-app.controller('MainCtrl', function ($scope, WeatherApi) {
-	//GOOGLE API
-	function initApi() {
-		var options = {
-			types: ['(cities)']
-		};
-		var input = document.getElementById('searchCityName');
-		var autocomplete = new google.maps.places.Autocomplete(input, options);
-		google.maps.event.addListener(autocomplete, 'place_changed', function () {
-			var place = autocomplete.getPlace();
-			WeatherApi.getLoc().success(function (data) {
-				var lat = 'lat=' + place.geometry.location.lat(),
-					lon = '&lon=' + place.geometry.location.lng();
-				WeatherApi.getWeather(lat, lon).success(function (data) {
-					$scope.city = data.name;
-					var iconKey = data.weather[0].main;
+            return $http.jsonp(weatherApiUrl, requestConfig);
+        };
+        return obj;
+    }
 
-					$scope.getIcon = function () {
-						if (icons[iconKey]) {
-							if (iconKey === 'Clear') {
-								return icons[iconKey][dayOrNight()];
-							}
-							return icons[iconKey];
-						}
-					};
+    function MainCtrl($scope, WeatherApi) {
+        var currentGooglePlace = {},
+            currentLocation = {},
+            currentDegreeSystem = degreeSystem.cel;
 
-					cityWeather(data)
-				});
-			});
+        $scope.weatherIcon = "";
+        $scope.city = "";
+        $scope.country = "";
+        $scope.currentDegreeSystem = 'C';
+        $scope.changeUnit = changeUnit;
 
-		});
-	};
-	google.maps.event.addDomListener(window, 'load', initApi);
-	//****
-	//****
-	var helper = document.getElementById('searchCityName')
-	helper.oninput = function () {
-		document.getElementById('help').style.display = 'none';
-	};
-	//***
-	var icons = {
-		'Clear': {
-			'day': "wi wi-day-sunny",
-			'night': "wi wi-night-clear"
-		},
-		'Clouds': "wi wi-cloudy",
-		'Snow': "wi wi-snow",
-		'Rain': "wi wi-rain",
-		'Drizzle': "wi wi-sprinkle",
-		'Thunderstorm': "wi wi-thunderstorm"
-	};
+        google.maps.event.addDomListener(window, 'load', initApi);
+        WeatherApi.getLoc().success(function (data) {
+            var splitted = data.loc.split(',');
+            currentLocation.lat = splitted[0];
+            currentLocation.lon = splitted[1];
+            $scope.country = data.country;
+            WeatherApi.getWeather(currentLocation.lat, currentLocation.lon, $scope.currentDegreeSystem).success(onWeatherReceived);
+        });
 
-	$scope.unit = 'C';
-	var isChanged = false;
-	WeatherApi.getLoc().success(function (data) {
+        //GOOGLE API
+        function initApi() {
+            var options = {
+                types: ['(cities)']
+            };
+            var input = document.getElementById('searchCityName');
+            var autocomplete = new google.maps.places.Autocomplete(input, options);
+            google.maps.event.addListener(autocomplete, 'place_changed', function () {
+                currentGooglePlace = autocomplete.getPlace();
+                currentLocation.lat = currentGooglePlace.geometry.location.lat();
+                currentLocation.lon = currentGooglePlace.geometry.location.lng();
+                WeatherApi.getWeather(currentLocation.lat, currentLocation.lon, $scope.currentDegreeSystem).success(onWeatherReceived);
+            });
+        };
 
-		var lat = 'lat=' + data.loc.split(',')[0],
-			lon = '&lon=' + data.loc.split(',')[1];
+        function onWeatherReceived(data) {
+            var iconKey = data.weather[0].main;
+            $scope.city = data.name;
+            $scope.country = data.sys.country;
+            $scope.temp = Math.round(data.main.temp);
+            $scope.weatherIcon = getIcon(iconKey);
+        };
 
-		$scope.country = data.country;
-		WeatherApi.getWeather(lat, lon).success(function (data) {
-			$scope.city = data.name;
-			var iconKey = data.weather[0].main;
+        function changeUnit() {
+            $scope.currentDegreeSystem = $scope.currentDegreeSystem === degreeSystem.cel ? degreeSystem.fah : degreeSystem.cel;
+            WeatherApi.getWeather(currentLocation.lat, currentLocation.lon, $scope.currentDegreeSystem).success(onWeatherReceived);
+        };
 
-			$scope.getIcon = function () {
-				if (icons[iconKey]) {
-					if (iconKey === 'Clear') {
-						return icons[iconKey][dayOrNight()];
-					}
-					return icons[iconKey];
-				}
-			};
+        function getIcon(iconKey) {
+            if (icons[iconKey]) {
+                if (iconKey === 'Clear') {
+                    return icons[iconKey][utils.isDay() ? 'day' : 'night'];
+                }
+                return icons[iconKey];
+            }
+        };
+    };
+    var utils = {
+        isDay: function () {
+            var hours = new Date().getHours();
+            if (hours > 6 && hours < 20) {
+                return true;
+            }
+            return false;
+        }
+    };
 
-			cityWeather(data)
-		});
-	});
+    var icons = {
+        'Clear': {
+            'day': "wi wi-day-sunny",
+            'night': "wi wi-night-clear"
+        },
+        'Clouds': "wi wi-cloudy",
+        'Snow': "wi wi-snow",
+        'Rain': "wi wi-rain",
+        'Drizzle': "wi wi-sprinkle",
+        'Thunderstorm': "wi wi-thunderstorm"
+    };
 
-	function cityWeather(data) {
-		$scope.temp = $scope.cel = Math.round(data.main.temp);
-		$scope.fah = Math.round(($scope.temp * 9) / 5 + 32);
-	}
+    var degreeSystem = {
+        cel: 'C',
+        fah: 'F'
+    };
 
-	$scope.changeUnit = function () {
-		if (isChanged) {
-			$scope.unit = 'C';
-			$scope.temp = $scope.cel;
-			return isChanged = false;
-		}
-		$scope.unit = 'F';
-		$scope.temp = $scope.fah;
-		return isChanged = true;
-	};
-
-	function dayOrNight() {
-		var hours = new Date().getHours();
-		if (hours > 6 && hours < 20) {
-			return 'day';
-		}
-		return 'night';
-	}
-
-});
+})();
---
+++ readme.md
@@ -0,0 +1,4 @@
+#**Angular Weather App**
+### use:
+ - [Open weather map API](http://openweathermap.org/current)
+ - [Google Maps API Place Autocomplete](https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete?)
